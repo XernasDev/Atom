fbe1bb3a9a6de8979c79f05554d89c5d55ae4c6b
Xernas Dev <71571183+Xernas78@users.noreply.github.com>
2026-02-23 15:57:39 +0100

    Added lerp and clamp

--- src/main/java/dev/xernas/atom/math/MathUtils.java
+++ src/main/java/dev/xernas/atom/math/MathUtils.java
@@ -38,4 +38,12 @@ public class MathUtils {
         return sum / values.size();
     }
 
+    public static float clamp(float value, float min, float max) {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    public static float lerp(float start, float end, float t) {
+        return start + t * (end - start);
+    }
+
 }
